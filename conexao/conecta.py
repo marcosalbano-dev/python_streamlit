087c9710058e1ab0f9a306bd9f652561ad23c43a
Marcos Albano <marcos.telematica@gmail.com>
2023-07-17 16:52:44 -0300

    add diretorio conexao

---
+++ conexao/conecta.py
@@ -0,0 +1,15 @@
+﻿import psycopg2 as db
+import pandas as pd
+
+conn = db.connect(dbname='sige', user='marcos.albano', password='Albano@1971', port='5432', host='172.28.1.160')
+cursor = conn.cursor()
+
+cursor.execute('SELECT * FROM sige.lotes WHERE municipio_id = 2300101 LIMIT 10')
+resultado = cursor.fetchall()
+
+df = pd.DataFrame.from_dict(resultado)
+
+print(df)
+
+cursor.close()
+
