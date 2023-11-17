1a8dc587a9605776a539cfe5a29a3d4775ccb781
Marcos Albano <marcos.telematica@gmail.com>
2023-11-17 16:13:48 -0300

    Teste

--- utils.py
+++ utils.py
@@ -11,3 +11,5 @@ df_rec_estado = df.groupby('Local da compra')[['Preço']].sum()
 df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 
                 'lon',]].merge(df_rec_estado, left_on='Local da compra', 
                 right_index=True).sort_values('Preço', ascending=False)
+
+print(df_rec_estado)
