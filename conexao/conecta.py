a42eaee156b9944dd56da437dae7517aa57dffeb
Marcos Albano <marcos.telematica@gmail.com>
2023-11-21 13:37:50 -0300

    fundamentos

--- conexao/conecta.py
+++ conexao/conecta.py
@@ -1,26 +1,25 @@
 ﻿import psycopg2 as db
 import pandas as pd
 
-conn = db.connect(dbname='sige', user='marcos.albano', password='Albano@1971', port='5432', host='172.28.1.160')
+conn = db.connect(dbname='sige', user='postgres', password='Axp@01fal12', port='5432', host='localhost')
 cursor = conn.cursor()
 
 cursor.execute(
-    """SELECT x.nom_territorio, x.nome_municipio, count(x.lote_id) AS total_titulados FROM ( 
-        SELECT DISTINCT ON (l.id) l.id AS lote_id, m.nome AS nome_municipio, terr.nom_territorio 
+    """SELECT DISTINCT ON (l.id) l.id AS lote_id, m.nome AS nome_municipio, terr.nom_territorio 
         FROM sige.titulos t JOIN sige.lotes l ON l.id = t.lote_id 
         JOIN ibge.municipios m ON m.id = l.municipio_id 
         JOIN nom.territorio_mun tm ON tm.cod_mun = m.id 
         JOIN nom.territorios terr ON terr.cod_territorio = tm.cod_territorio 
         JOIN sige.situacoes_juridicas sj ON sj.id = l.situacao_juridica_id 
         WHERE l.projeto_especial = false AND t.flag_cancelamento <> 'S' AND m.uf = 'CE'
-    ) x GROUP BY nome_municipio, nom_territorio ORDER BY nome_municipio"""
+    """
 )
 
 resultado = cursor.fetchall()
 
-df = pd.DataFrame.from_dict(resultado)
+df_sige = pd.DataFrame.from_dict(resultado)
 
-print(df)
+# print(df)
 
 cursor.close()
 
--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 from dataset import df
+import pandas as pd
+import streamlit as st
+import time
+
 
 def format_number(value, prefix=''):
     for unit in ['', 'mil']:
@@ -7,9 +11,37 @@ def format_number(value, prefix=''):
         value /= 1000
     return f'{prefix} {value:.2f} milhões'
 
-df_rec_estado = df.groupby('Local da compra')[['Preço']].sum()
-df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 
-                'lon',]].merge(df_rec_estado, left_on='Local da compra', 
-                right_index=True).sort_values('Preço', ascending=False)
 
-print(df_rec_estado)
+# 1 - Dataframe Receita por estado
+df_rec_estado = df.groupby('Local da compra')[['Preço']].sum()
+df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon',]].merge(
+    df_rec_estado, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
+
+# print(df_rec_estado)
+# 2 - Dataframe Receita Mensal
+df_rec_mensal = df.set_index('Data da Compra').groupby(
+    pd.Grouper(freq='M'))['Preço'].sum().reset_index()
+
+df_rec_mensal['Ano'] = df_rec_mensal['Data da Compra'].dt.year
+df_rec_mensal['Mes'] = df_rec_mensal['Data da Compra'].dt.month_name()
+# print(df_rec_mensal)
+
+# 3 - Dataframe Receita por Categoria
+df_rec_categoria = df.groupby('Categoria do Produto')[
+    ['Preço']].sum().sort_values('Preço', ascending=False)
+
+# 4 - Dataframe Vendedores
+df_vendedores = pd.DataFrame(df.groupby('Vendedor')['Preço'].agg(['sum', 'count']))
+# print(df_vendedores)
+
+# Função para converter arquivo csv
+@st.cache_data
+def convert_csv(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+def mensagem_sucesso():
+    success = st.success(
+        'Arquivo baixado com sucesso',
+    )
+    time.sleep(3)
+    success.empty()
